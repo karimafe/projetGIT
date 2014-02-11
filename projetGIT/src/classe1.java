e1a0a37e567c4301f964ed2269369b20c7d86f99
ikram <ikram@172.27.48.43>
2014-02-11 12:26:55 +0100

    commit4

--- projetGIT/src/classe1.java
+++ projetGIT/src/classe1.java
@@ -7,6 +7,11 @@ public class classe1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  System.out.println("modif1");
+
+ System.out.println("modif2");
+ System.out.println("modif3");
+ System.out.println("modif4");
+
 	}
 
 }
